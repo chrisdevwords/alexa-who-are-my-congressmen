6937d88cec9eba981694e743419f8daf29169392
Chris Edwards <chrisedwards@Chriss-MacBook-Pro.local>
2017-01-13 12:48:20 -0500

    adding a test

--- test/test_service-helper.js
+++ test/test_service-helper.js
@@ -8,3 +8,23 @@ const { beforeEach, afterEach, describe, it } = mocha;
 const { expect, config } = chai;
 
 config.includeStack = true;
+
+import getMembersByAddress from '../src/serivce-helper';
+
+describe('The Service Helper Methods', () => {
+
+    describe('Get member by Address', () => {
+
+        it('gets a member of congress by address', (done) => {
+            getMembersByAddress('45 Main St. Brooklyn')
+                .then(({ message }) => {
+                    expect(message).to.eq(
+                        'Your representative is Nydia M. Velázquez, ' +
+                        'and your senators are Kirsten E. Gillibrand and and Charles E. Schumer.'
+                    );
+                    done();
+                })
+                .catch(done);
+        });
+    });
+});
